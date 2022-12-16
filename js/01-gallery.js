266be6c1b3c44afde35a13f2bd98e01ff76acaf0
ArtemKos2022 <110256138+ArtemKos2022@users.noreply.github.com>
2022-12-16 16:01:44 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,3 +6,23 @@ console.log(galleryItems);
 const galleryContainer = document.querySelector(".gallery");
 const galleryMarkup = createGalleryMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend" , galleryMarkup )
+function createGalleryMarkup(items) {
+  return items.map(({preview, original, description}) => {
+  return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+  `;
+})
+.join("");
+}
+galleryContainer.addEventListener("click", onFullSizeImage)
+
+
