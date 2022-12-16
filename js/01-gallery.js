8430d943872bff0477df9a472b5ccfe5d037fc44
ArtemKos2022 <110256138+ArtemKos2022@users.noreply.github.com>
2022-12-16 15:44:09 +0200

    gallery-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend" , galleryMarkup )
